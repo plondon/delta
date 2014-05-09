24b8f950044a8ba4d2b51b5981897c1366209125
Shaun McGeever <shaunmcgeever@gmail.com>
2014-05-09 16:18:12 -0400

    added images directory, added two answers

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,8 @@ questions_text = ["How do I make an each loop in Javascript?",
 "How do I make variables in Javascript?",
 "How do I make an if-else loop in Javascript?",
 "How do I print text to the console in Javascript?",
-"How do I make equalities in Javascript?"]
+"How do I make equalities in Javascript?",
+"How do I count to ten?"]
 
 
 questions_text.each do |question|
@@ -29,3 +30,5 @@ end
 
 Answer.create(:user => sherif, question: Question.find_by_query("How do I make an if-else loop in Javascript?"), original_solution: "http://i.imgur.com/wVw9DFj.png", final_solution: "http://i.imgur.com/B6JL4NA.png", delta: "http://i.imgur.com/2qmKaks.gif", votes: 10)
 Answer.create(:user => sherif, question: Question.find_by_query("How do I make a Class object in Javascript?"), original_solution: "http://i.imgur.com/6wHXqQo.png", final_solution: "http://i.imgur.com/nW6hdKj.png", delta: "http://i.imgur.com/WkkMGwP.gif", votes: 10)
+Answer.create(:user => nate, question: Question.find_by_query("How do I make method in Javascript?"), original_solution: "http://i.imgur.com/k5r2Zce.png", final_solution: "http://i.imgur.com/YwiUcG9.png", delta: "http://i.imgur.com/bXuYkZm.gif", comments: "Methods are split into to two when using javascript. Methods can become functions, but when dealing with a Class or Object they are called protoype functions. Prototype functions are called like 'number.multiply_by_two'.", votes: 7)
+Answer.create(:user => nate, question: Question.find_by_query("How do I make map loop in Javascript?"), original_solution: "http://i.imgur.com/os9ilsl.png", final_solution: "http://i.imgur.com/LIaj4MY.png", delta: "http://i.imgur.com/tafY7oc.gif", votes: 12)
