190611d2da64ff83a32d741290fa9066e4ff4779
plondon <plondon@gm.slc.edu>
2014-05-10 22:18:14 -0400

    validate unique solutions

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   validates :original_solution, :final_solution, presence: true
+  validates :original_solution, :final_solution, :delta, uniqueness: true
 end
